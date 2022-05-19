7e70c0868ef47080212d2a6100fd1f4b68202411
Annie <annie.li.29122@gmail.com>
2022-05-19 12:36:22 -0400

    lol

--- Parser.java
+++ Parser.java
@@ -16,10 +16,14 @@ public class Parser {
 		}
 		return subset;
 	}
+
 	public Expression parse(ArrayList<String> tokens) throws ParseException {
 		String last = tokens.get(tokens.size() - 1);
+		String first = tokens.get(0);
+		System.out.println("Parsng" + tokens);
 
 		if (last.equals(")")) {
+
 			int closedParensInWay = -1;
 			for (int i = tokens.size() - 1; i >= 0; i--) {
 				if (tokens.get(i).equals(")")) {
@@ -30,8 +34,8 @@ public class Parser {
 							return parse(subArrayList(tokens, 1, tokens.size() - 1));
 						} else {
 							return new Application(parse(subArrayList(tokens, 0, i)),
-							parse(subArrayList(tokens, i + 1, tokens.size() - 1)));	
-						}				
+									parse(subArrayList(tokens, i + 1, tokens.size() - 1)));
+						}
 					} else {
 						closedParensInWay--;
 					}
@@ -40,13 +44,14 @@ public class Parser {
 
 			throw new ParseException("Parentheses not balanced! :()", 0);
 
-		}
-
-		if (tokens.size() == 1) {
+		} else if (tokens.size() == 1) {
 			// last remaining is letter
 			if (last.charAt(0) >= 'a' && last.charAt(0) <= 'z' || last.charAt(0) >= 'A' && last.charAt(0) <= 'Z') {
 				return new Variable(last);
 			}
+		} else if (first.equals("λ")) {
+			System.out.println(subArrayList(tokens, 3, tokens.size()));
+			return new Function(new Variable(tokens.get(1)), parse(subArrayList(tokens, 3, tokens.size())));
 		}
 
 		return new Application(parse(subArrayList(tokens, 0, tokens.size() - 1)),
--- Variable.java
+++ Variable.java
@@ -1,11 +1,12 @@
 
 public class Variable implements Expression {
+
 	private String name;
-	
+
 	public Variable(String name) {
 		this.name = name;
 	}
-	
+
 	public String toString() {
 		return name;
 	}
