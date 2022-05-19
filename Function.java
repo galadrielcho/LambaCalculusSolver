e88e9e671240cdb84c644abee4894de4bc34ba85
Annie <annie.li.29122@gmail.com>
2022-05-19 12:36:17 -0400

    Co-authored-by: Galadriel Cho <galadrielcho@users.noreply.github.com>

--- Function.java
+++ Function.java
@@ -1,4 +1,16 @@
 
 public class Function implements Expression {
 
+    public Variable v;
+    public Expression e;
+
+    public Function(Variable v, Expression e) {
+        this.v = v;
+        this.e = e;
+    }
+
+    public String toString() {
+        return String.format("(λ%s.%s)", v, e);
+    }
+
 }
